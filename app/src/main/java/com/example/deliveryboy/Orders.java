a5a7e0b2e5f818e140e307e8404f4ae5ca0605cb
Athul Gopalakrishnan <athulgopalakrishnan@Athuls-MacBook-Air.local>
2021-07-03 20:48:08 +0530

    Service structure added

--- app/src/main/java/com/example/deliveryboy/Orders.java
+++ app/src/main/java/com/example/deliveryboy/Orders.java
@@ -2,33 +2,25 @@ package com.example.deliveryboy;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
-import android.app.Dialog;
-import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.TextView;
-
+import android.widget.CompoundButton;
 import com.example.deliveryboy.Adapters.OrderViewAdapter;
 import com.example.deliveryboy.PojoClasses.OrderDetails;
 import com.google.android.material.progressindicator.LinearProgressIndicator;
+import com.google.android.material.switchmaterial.SwitchMaterial;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-
-import org.w3c.dom.Text;
-
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class Orders extends AppCompatActivity {
@@ -41,7 +33,8 @@ public class Orders extends AppCompatActivity {
     private OrderViewAdapter orderViewAdapter;
     private List<String> orderKeys , dishKeys;
     private LinearProgressIndicator linearProgressIndicator;
-    private ChildEventListener childEventListener;
+    private SwitchMaterial switchMaterial;
+    private Intent serviceIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,30 +52,27 @@ public class Orders extends AppCompatActivity {
         linearProgressIndicator.setVisibility(View.INVISIBLE);
         orderViewAdapter = new OrderViewAdapter(getApplicationContext(), orderList , orderKeys);
         recyclerView.setAdapter(orderViewAdapter);
-
+        switchMaterial=findViewById(R.id.switch1);
+        retrieveOrders();
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.d("KEPLER", "On Start Called");
+    private void retrieveOrders() {
         linearProgressIndicator.setVisibility(View.VISIBLE);
         fireBaseRealtimeDatabase.addChildEventListener(new ChildEventListener() {
             @Override
             public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
-                OrderDetails orderDetails = snapshot.getValue(OrderDetails.class);
-                orderList.add(orderDetails);
-                orderKeys.add(snapshot.getKey());
-                Collections.reverse(orderList);
-                Collections.reverse(orderKeys);
-                orderViewAdapter.notifyDataSetChanged();
                 linearProgressIndicator.setVisibility(View.INVISIBLE);
+                OrderDetails orderDetails = snapshot.getValue(OrderDetails.class);
+                orderList.add(0,orderDetails);
+                orderKeys.add(0,snapshot.getKey());
+                orderViewAdapter.notifyDataSetChanged();
+
             }
 
             @Override
             public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
-
-
+                orderList.set(orderKeys.indexOf(snapshot.getKey()),snapshot.getValue(OrderDetails.class));
+                orderViewAdapter.notifyDataSetChanged();
             }
 
             @Override
@@ -101,6 +91,22 @@ public class Orders extends AppCompatActivity {
             }
         });
 
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        serviceIntent=new Intent(Orders.this,NotifyService.class);
+        switchMaterial.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    startService(serviceIntent);
+                }else{
+                    stopService(serviceIntent);
+                }
+            }
+        });
 
     }
 
