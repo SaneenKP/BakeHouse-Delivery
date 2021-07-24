04b849a3123d1216ddba6272d867abf4d4d5c8ac
saneenKP <saneenkp2000@gmail.com>
2021-07-24 21:25:48 +0530

    database deliveryboy node creation with temperory data implemented while anonymous signin

--- app/src/main/java/com/example/deliveryboy/MainActivity.java
+++ app/src/main/java/com/example/deliveryboy/MainActivity.java
@@ -9,11 +9,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Toast;
 
+import com.example.deliveryboy.PojoClasses.DeliveryBoyDetails;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
@@ -34,16 +36,32 @@ public class MainActivity extends AppCompatActivity {
 
     private void AnonymousSignIn(){
 
+        DatabaseReference deliveryBoyDatabase = FirebaseDatabase.getInstance().getReference().child(getApplicationContext().getResources().getString(R.string.DeliveryBoyNode));
         alertDialog = customAlertDialog.showAlertDialog();
          auth.signInAnonymously().
                  addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
              @Override
              public void onComplete(@NonNull Task<AuthResult> task) {
                     if (task.isSuccessful()){
-                        Toast.makeText(getApplicationContext() , ""+task.getResult().getUser().getUid() , Toast.LENGTH_LONG).show();
-                        Intent openOrders = new Intent(MainActivity.this , Orders.class);
-                        alertDialog.dismiss();
-                        startActivity(openOrders);
+
+                        String userID = task.getResult().getUser().getUid();
+                        DeliveryBoyDetails deliveryBoyDetails = new DeliveryBoyDetails();
+                        deliveryBoyDetails.setName("temp");
+                        deliveryBoyDetails.setNumber("0000000000");
+                        deliveryBoyDatabase.child(userID).setValue(deliveryBoyDetails).addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull  Task<Void> task) {
+                                if (task.isSuccessful()){
+                                    Toast.makeText(getApplicationContext() , ""+userID , Toast.LENGTH_LONG).show();
+                                    Intent openOrders = new Intent(MainActivity.this , Orders.class);
+                                    alertDialog.dismiss();
+                                    startActivity(openOrders);
+                                }else{
+                                    Toast.makeText(getApplicationContext() , "Failed To upload Data" , Toast.LENGTH_LONG).show();
+                                }
+                            }
+                        });
+
                     }else{
                         Toast.makeText(getApplicationContext() , "Anonymous Authentication Failed" , Toast.LENGTH_LONG).show();
                     }
